592636f38c97f603ccb3b313b3fbd3c55f7d89f4
Kraken <sufranj.naweed@gmail.com>
2019-07-13 00:23:02 +0200

    update : send data to movie details from  movie db

--- app.js
+++ app.js
@@ -34,20 +34,20 @@ app.get('/movie-details/:id', (req, res) => {
     const id =  req.params.id;
     requestMovie(id)
         .then((data) =>{
-            console.log(data.title);
-            res.json(data);
-            /*
+            console.log(data);
+            //res.json(data);
             res.render("movie-details", {
-                title : data.title
+                data : data
             });
-            */
         })
 });
 
 const requestMovie = (id) => {
-    return  axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${dotenv.parsed.API_KEY}`)
+    const query = `https://api.themoviedb.org/3/movie/${id}?api_key=${dotenv.parsed.API_KEY}`;
+    return  axios.get(query)
                 .then((res) => {
-                    const data = res.data;
+                    const data = dataMovie(res.data, id);
+                  
                     return data;
                 })
                 .catch((err) => {
@@ -55,7 +55,54 @@ const requestMovie = (id) => {
                 });
 }
 
+const dataMovie = (data, id) => {
+    /*
+        const trailer = trailerFromYoutube(id)
+        .then((res) => {
+        console.log(finalData.trailer);
+            return finalData;
+        })
+        .catch((err) => {
+            console.error(err);
+        });
 
+    */
+    const finalData = {
+        adult               : data.adult, 
+        title               : data.title,
+        homepage            : data.homepage,
+        budget              : data.budget,
+        genres              : data.genres,
+        poster              : "https://image.tmdb.org/t/p/w300_and_h450_bestv2" + data.poster_path,
+        backgroundPoster    : "https://image.tmdb.org/t/p/w1400_and_h450_face"+ data.poster_path,
+        overview            : data.overview,
+        productionCompanies : data.production_companies,
+       
+    };
+
+    finalData.release_date = getDateYear(data.release_date);
+
+    return finalData;
+}
+
+const getDateYear = (date) => {
+    let newDate = new Date(date);
+    var result = newDate.getFullYear();
+
+    return result;
+}
+
+const trailerFromYoutube = (id) => {
+    const query = `http://api.themoviedb.org/3/movie/${id}/videos?api_key=${dotenv.parsed.API_KEY}`;
+    return axios.get(query)
+        .then((res) => {
+            const video = `https://www.youtube-nocookie.com/embed/${res.data.results[0].key}]?controls=0`;
+            return video;
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+}
 
 //////////////
 /// POST
